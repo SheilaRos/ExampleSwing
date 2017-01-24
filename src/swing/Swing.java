b359716a033cd833aacfcf92db1740561c853303
SheilaRos <sheila.ros.cheme@gmail.com>
2017-01-24 17:33:28 +0100

    EjemploSwingMar

---
+++ src/swing/Swing.java
@@ -0,0 +1,10 @@
+package swing;
+/* La primera ventana será JFrame*/
+public class Swing {
+    public static void main(String[] args) {
+       Principal principal = new Principal();
+       principal.setLocationRelativeTo(null);
+       principal.setVisible(true);
+    }
+    
+}
